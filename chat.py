cf367092edab2f046a74bf56dd1c178e93c5e414
Tien Huynh <huynhngoctiendlvn@gmail.com>
2021-08-04 02:26:05 -0500

    adding GUI

--- chat.py
+++ chat.py
@@ -24,13 +24,9 @@ model.load_state_dict(model_state)
 model.eval()
 
 bot_name = "Loki"
-print("Let's chat! type 'quit' to exit")
-while(True):
-    sentence = input('You: ')
-    if sentence == "quit":
-        break
-    
-    sentence = tokenize(sentence)
+
+def get_response(message):
+    sentence = tokenize(message)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X)
@@ -42,12 +38,16 @@ while(True):
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
-    if prob.item() > 0.80:
-        print(prob)
+    if prob.item() > 0.75:
+        #print(prob)
         for intent in intents["intents"]:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])} ")
+                return random.choice(intent['responses'])
 
-    else:
-        print(f"{bot_name}: I do not understand")
+
+    return "I do not understand"
+
+
+    
+   
 
